bed8ee9ab3447d0178b1aefbf0cd63db59f38907
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-07-19 10:25:53 -0700

    Deploy script update.

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -39,8 +39,8 @@ do
   echo "------- Running kubectl get pods -------"
   PODS=$(kubectl get pods | awk 'NR>1 {print $0}')
   echo "$PODS"
-  NUM_AIRFLOW_READY=$(echo $PODS | grep airflow | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
-  NUM_POSTGRES_READY=$(echo $PODS | grep postgres | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
+  NUM_AIRFLOW_READY=$(echo $PODS | grep ^airflow | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
+  NUM_POSTGRES_READY=$(echo $PODS | grep ^postgres | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
   if [ "$NUM_AIRFLOW_READY" == "2" ] && [ "$NUM_POSTGRES_READY" == "1" ]; then
     break
   fi
