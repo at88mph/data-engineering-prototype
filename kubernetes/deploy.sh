8adcc0c8592de439940a80b1da34c679914a2914
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-07-19 10:19:25 -0700

    Wait for both pods.

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -41,7 +41,7 @@ do
   echo "$PODS"
   NUM_AIRFLOW_READY=$(echo $PODS | grep airflow | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
   NUM_POSTGRES_READY=$(echo $PODS | grep postgres | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
-  if [ "$NUM_AIRFLOW_READY" == "1" ] && [ "$NUM_POSTGRES_READY" == "1" ]; then
+  if [ "$NUM_AIRFLOW_READY" == "2" ] && [ "$NUM_POSTGRES_READY" == "1" ]; then
     break
   fi
   sleep 4
