0aefba407d49b9bce2824430391543db35fc331c
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-07-23 13:00:55 -0700

    Recombine web server and scheduler to attempt fixing some weird errors.

---
+++ kubernetes/deploy-wait-single.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#  Licensed to the Apache Software Foundation (ASF) under one   *
+#  or more contributor license agreements.  See the NOTICE file *
+#  distributed with this work for additional information        *
+#  regarding copyright ownership.  The ASF licenses this file   *
+#  to you under the Apache License, Version 2.0 (the            *
+#  "License"); you may not use this file except in compliance   *
+#  with the License.  You may obtain a copy of the License at   *
+#                                                               *
+#    http://www.apache.org/licenses/LICENSE-2.0                 *
+#                                                               *
+#  Unless required by applicable law or agreed to in writing,   *
+#  software distributed under the License is distributed on an  *
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+#  KIND, either express or implied.  See the License for the    *
+#  specific language governing permissions and limitations      *
+#  under the License.                                           *
+
+# Wait for up to 10 minutes for everything to be deployed
+for i in {1..150}
+do
+  echo "------- Running kubectl get pods -------"
+  PODS=$(kubectl get pods | awk 'NR>1 {print $0}')
+  echo "$PODS"
+  NUM_AIRFLOW_READY=$(echo $PODS | grep airflow | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
+  NUM_POSTGRES_READY=$(echo $PODS | grep postgres-airflow | awk '{print $2}' | grep -E '([0-9])\/(\1)' | wc -l | xargs)
+  if [ "$NUM_AIRFLOW_READY" == "2" ] && [ "$NUM_POSTGRES_READY" == "1" ]; then
+    break
+  else
+    echo "Airflow (Scheduler + Web Server) instance count is $NUM_AIRFLOW_READY"
+    echo "PostgreSQL instance count is $NUM_POSTGRES_READY"
+  fi
+  sleep 4
+done
+
+AIRFLOW_POD=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | grep airflow | head -1)
+
+echo "------- Airflow (Scheduler + Web Server) Pod description -------"
+kubectl describe pod $AIRFLOW_POD
+echo "------- webserver logs -------"
+kubectl logs $AIRFLOW_POD webserver
+echo "------- scheduler logs -------"
+kubectl logs $AIRFLOW_POD scheduler
+echo "--------------"
---
+++ kubernetes/remove-kube-single.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIRNAME=$(cd "$(dirname "$0")"; pwd)
+
+kubectl delete -f $DIRNAME/postgres.yaml
+kubectl delete -f $DIRNAME/airflow.yaml
+kubectl delete -f $DIRNAME/secrets.yaml
+
+# Extra cleanup for left over pods.
+kubectl delete deployments --all
+kubectl delete pods --all
+
+echo "Pods:"
+kubectl get pods
+
+echo "Services: "
+kubectl get services
+
+echo "Deployments: "
+kubectl get deployments
