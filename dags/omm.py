0d0bb0b8b37147cb781f09b78b87b36cd0f59c0b
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-07-26 11:21:43 -0700

    DAG Name with start date.

--- dags/omm.py
+++ dags/omm.py
@@ -40,7 +40,7 @@ default_args = {
     'provide_context': True
 }
 
-dag = DAG(dag_id='{}.{}'.format(PARENT_DAG_NAME, default_args['start_date']), default_args=default_args, schedule_interval=None)
+dag = DAG(dag_id='{}.{}'.format(PARENT_DAG_NAME, default_args['start_date'].isoformat()), default_args=default_args, schedule_interval=None)
 
 def extract(**kwargs):    
     logging.info('Populating inputs.')
