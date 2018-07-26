7f9eb8078a9a2160e34bfaa97c4a46e72795163b
at88mph <cyclodaddy@gmail.com>
2018-07-25 21:05:06 -0700

    Re-added subdag function

--- dags/omm.py
+++ dags/omm.py
@@ -13,7 +13,6 @@ from airflow.models import DAG, Variable
 from airflow.hooks.http_hook import HttpHook
 from airflow.contrib.hooks.redis_hook import RedisHook
 
-from omm_subdag import sub_dag
 from datetime import datetime, timedelta
 from urllib import parse as parse
 from urllib import request as request
@@ -68,12 +67,42 @@ def populate_inputs(**kwargs):
     logging.info('Sleeping...')
     time.sleep(3)
 
-    return 'Finished inserting {} items into Redis.'.format(count)        
+    return 'Finished inserting {} items into Redis.'.format(count)
+
+def sub_dag(parent_dag_name, child_dag_name, start_date, schedule_interval, redis_list_name):   
+    sub_dag = DAG(
+      '{}.{}'.format(parent_dag_name, child_dag_name),
+      schedule_interval=schedule_interval,
+      start_date=datetime(2019, 11, 25),
+    )
+    redis = RedisHook(redis_conn_id='redis_default')
+    redis_conn = redis.get_conn()
+    start_sub_dag = DummyOperator(task_id='{}.start'.format(sub_dag.dag_id), dag=sub_dag)
+    complete_sub_dag = DummyOperator(task_id='{}.complete'.format(sub_dag.dag_id), dag=sub_dag)
+    logging.info('Looping items.')
+    uri_key = redis_conn.rpop(redis_list_name)
+    while uri_key:
+        decoded_key = uri_key.decode('utf-8')
+        logging.info('Next key: {}'.format(decoded_key))
+        task = KubernetesPodOperator(
+                 namespace='default',
+                 task_id='{}.{}'.format(sub_dag.dag_id, decoded_key),
+                 image='ubuntu:18.10',
+                 in_cluster=True,
+                 get_logs=True,
+                 cmds=['echo'],
+                 arguments=[decoded_key],
+                 name='airflow-test-pod',
+                 dag=sub_dag)
+        uri_key = redis_conn.rpop(redis_list_name)
+        start_sub_dag >> task >> complete_sub_dag
+
+    return sub_dag
 
 # start = DummyOperator(task_id='start', dag=dag)
+sub_dag_pointer = sub_dag
 start = PythonOperator(task_id='populate_inputs', python_callable=populate_inputs, dag=dag)
-sub_dag_operator = SubDagOperator(subdag=sub_dag(PARENT_DAG_NAME, CHILD_DAG_NAME, dag.start_date, dag.schedule_interval, REDIS_LIST_NAME),
-                                  task_id=CHILD_DAG_NAME, dag=dag)
+sub_dag_operator = SubDagOperator(subdag=sub_dag_pointer(PARENT_DAG_NAME, CHILD_DAG_NAME, dag.start_date, dag.schedule_interval, REDIS_LIST_NAME), task_id=CHILD_DAG_NAME, dag=dag)
 complete = DummyOperator(task_id='complete', dag=dag)
 
 start >> sub_dag_operator >> complete
