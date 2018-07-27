305488284a6eb52a7bf3c68663588d4b53c8bc3c
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-07-27 15:19:25 -0700

    Rework to use one file.

--- dags/omm.py
+++ dags/omm.py
@@ -9,18 +9,16 @@ from airflow.contrib.kubernetes.volume import Volume
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.contrib.sensors.redis_key_sensor import RedisKeySensor
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.models import DAG
-from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.models import DAG, Variable
 
 from datetime import datetime, timedelta
-from urllib import parse as parse
-from urllib import request as request
+from urllib import parse, request
 
 
-PARENT_DAG_NAME = 'omm_dag'
-CHILD_DAG_NAME = 'run_omm_instance'
+INPUT_FILE = Variable.get('input_file_uri')
+(_, _, path, _, _, _) = parse(INPUT_FILE)
+PARENT_DAG_NAME = 'omm_dag_{}'.format(path.replace('+', '_').replace('/', '__'))
 
 config = {'working_directory': '/root/airflow',
           'resource_id': 'ivo://cadc.nrc.ca/sc2repo',
@@ -66,68 +64,23 @@ logs_volume = Volume(name='airflow-logs', configs=logs_volume_config)
 
 dag = DAG(dag_id='{}.{}'.format(PARENT_DAG_NAME, default_args['start_date'].strftime(
     "%Y-%m-%d_%H_%M_%S")), catchup=True, default_args=default_args, schedule_interval=None)
-redis_key = 'omm_snapshot'
-
-
-def extract(**kwargs):
-    logging.info('Extracting inputs.')
-    redis = RedisHook(redis_conn_id='redis_default')
-    uri_keys = redis.get_conn().lrange(redis_key, 0, -1)
-    logging.info('Extracted {} items'.format(len(uri_keys)))
-    return uri_keys
-
-
-def transform_and_load(uri, dag, **kwargs):
-    decoded_key = uri.decode('utf-8')
-    logging.info('Next key: {}'.format(decoded_key))
-    return KubernetesPodOperator(
-        namespace='default',
-        task_id='transform',
-        image='ubuntu:18.10',
-        in_cluster=True,
-        get_logs=True,
-        cmds=['echo'],
-        arguments=['{}.{}'.format(dag.dag_id, uri)],
-        volume_mounts=[dags_volume_mount, logs_volume_mount],
-        volumes=[dags_volume, logs_volume],
-        name='airflow-test-pod',
-        dag=dag)
-
-
-def load(**kwargs):
-    redis = RedisHook(redis_conn_id='redis_default')
-    redis.get_conn().delete(redis_key)
-    return 'Loaded {}.'.format(redis_key)
-
 
 with dag:
-    # start_op = DummyOperator(task_id='start_dag', dag=dag)
-    start_op = RedisKeySensor(
-        task_id='redis_key_check',
-        redis_conn_id='redis_default',
-        poke_interval=5,
-        timeout=5,
-        soft_fail=True,
-        dag=dag,
-        key=redis_key)
+    start_op = DummyOperator(task_id='start_dag', dag=dag)
+
+    transform_op = KubernetesPodOperator(
+                namespace='default',
+                task_id='transform',
+                image='ubuntu:18.10',
+                in_cluster=True,
+                get_logs=True,
+                cmds=['echo'],
+                arguments=[INPUT_FILE],
+                volume_mounts=[dags_volume_mount, logs_volume_mount],
+                volumes=[dags_volume, logs_volume],
+                name='airflow-test-pod',
+                dag=dag)
+
     complete_op = DummyOperator(task_id='complete_dag', dag=dag)
 
-    complete_op.set_upstream(start_op)
-    
-    # extract_op = PythonOperator(task_id='extract', python_callable=extract, dag=dag)
-    # transform_op = KubernetesPodOperator(
-    #             namespace='default',
-    #             task_id='transform',
-    #             image='ubuntu:18.10',
-    #             in_cluster=True,
-    #             get_logs=True,
-    #             cmds=['echo'],
-    #             arguments=[dag.dag_id],
-    #             volume_mounts=[dags_volume_mount, logs_volume_mount],
-    #             volumes=[dags_volume, logs_volume],
-    #             name='airflow-test-pod',
-    #             dag=dag)
-    # for uri in uris:
-    #     start_op >> transform_and_load(uri, dag) >> complete_op
-    # transform_op = PythonOperator(task_id='transform', python_callable=print_uris, dag=dag)
-    # load_op = PythonOperator(task_id='load', python_callable=load, dag=dag)
+    start_op >> transform_op >> complete_op
