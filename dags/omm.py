c13d2434ce73e2048fce40bf0267acd6a34095ae
at88mph <cyclodaddy@gmail.com>
2018-07-29 15:26:28 -0700

    Fix volume mounts.

--- dags/omm.py
+++ dags/omm.py
@@ -6,7 +6,6 @@ import time
 import re
 
 from airflow.contrib.kubernetes.volume_mount import VolumeMount
-from airflow.contrib.kubernetes.volume import Volume
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
@@ -45,25 +44,11 @@ dags_volume_mount = VolumeMount('airflow-dags',
                                 mount_path='/root/airflow/dags',
                                 sub_path=None,
                                 read_only=True)
-dags_volume_config = {
-    'persistentVolumeClaim':
-        {
-            'claimName': 'airflow-dags'
-        }
-}
-dags_volume = Volume(name='airflow-dags', configs=dags_volume_config)
 
 logs_volume_mount = VolumeMount('airflow-logs',
                                 mount_path='/root/airflow/logs',
                                 sub_path=None,
                                 read_only=True)
-logs_volume_config = {
-    'persistentVolumeClaim':
-        {
-            'claimName': 'airflow-logs'
-        }
-}
-logs_volume = Volume(name='airflow-logs', configs=logs_volume_config)
 
 dag = DAG(dag_id='{}.{}'.format(PARENT_DAG_NAME, default_args['start_date'].strftime(
     "%Y-%m-%d_%H_%M_%S")), catchup=True, default_args=default_args, schedule_interval=None)
@@ -80,7 +65,6 @@ with dag:
                 cmds=['echo'],
                 arguments=['science_file_{}'.format(INPUT_FILE)],
                 volume_mounts=[dags_volume_mount, logs_volume_mount],
-                volumes=[dags_volume, logs_volume],
                 name='airflow-omm_science_file-pod',
                 dag=dag)
 
@@ -93,7 +77,6 @@ with dag:
                 cmds=['echo'],
                 arguments=['preview_{}'.format(INPUT_FILE)],
                 volume_mounts=[dags_volume_mount, logs_volume_mount],
-                volumes=[dags_volume, logs_volume],
                 name='airflow-omm_preview-pod',
                 dag=dag)
 
@@ -106,7 +89,6 @@ with dag:
                 cmds=['echo'],
                 arguments=['thumbnail_{}'.format(INPUT_FILE)],
                 volume_mounts=[dags_volume_mount, logs_volume_mount],
-                volumes=[dags_volume, logs_volume],
                 name='airflow-omm_thumbnail-pod',
                 dag=dag)                
 
--- dags/vlass.py
+++ dags/vlass.py
@@ -6,7 +6,6 @@ import time
 import re
 
 from airflow.contrib.kubernetes.volume_mount import VolumeMount
-from airflow.contrib.kubernetes.volume import Volume
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
@@ -47,25 +46,11 @@ dags_volume_mount = VolumeMount('airflow-dags',
                                 mount_path='/root/airflow/dags',
                                 sub_path=None,
                                 read_only=True)
-dags_volume_config = {
-    'persistentVolumeClaim':
-        {
-            'claimName': 'airflow-dags'
-        }
-}
-dags_volume = Volume(name='airflow-dags', configs=dags_volume_config)
 
 logs_volume_mount = VolumeMount('airflow-logs',
                                 mount_path='/root/airflow/logs',
                                 sub_path=None,
                                 read_only=True)
-logs_volume_config = {
-    'persistentVolumeClaim':
-        {
-            'claimName': 'airflow-logs'
-        }
-}
-logs_volume = Volume(name='airflow-logs', configs=logs_volume_config)
 
 dag = DAG(dag_id='{}.{}'.format(PARENT_DAG_NAME, default_args['start_date'].strftime(
     "%Y-%m-%d_%H_%M_%S")), catchup=True, default_args=default_args, schedule_interval=None)
@@ -82,7 +67,6 @@ with dag:
                 cmds=['echo'],
                 arguments=['science_file_{}'.format(INPUT_FILE)],
                 volume_mounts=[dags_volume_mount, logs_volume_mount],
-                volumes=[dags_volume, logs_volume],
                 name='airflow-vlass_science_file-pod',
                 dag=dag)
 
@@ -95,7 +79,6 @@ with dag:
                 cmds=['echo'],
                 arguments=['noise_{}'.format(NOISE_FILE)],
                 volume_mounts=[dags_volume_mount, logs_volume_mount],
-                volumes=[dags_volume, logs_volume],
                 name='airflow-vlass_noise-pod',
                 dag=dag)
 
@@ -108,7 +91,6 @@ with dag:
                 cmds=['echo'],
                 arguments=['preview_{}'.format(INPUT_FILE)],
                 volume_mounts=[dags_volume_mount, logs_volume_mount],
-                volumes=[dags_volume, logs_volume],
                 name='airflow-vlass_preview-pod',
                 dag=dag)
 
@@ -121,7 +103,6 @@ with dag:
                 cmds=['echo'],
                 arguments=['thumbnail_{}'.format(INPUT_FILE)],
                 volume_mounts=[dags_volume_mount, logs_volume_mount],
-                volumes=[dags_volume, logs_volume],
                 name='airflow-vlass_thumbnail-pod',
                 dag=dag)                
 
