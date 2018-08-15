9ce5dddd3b84998f848f97a88481c841b68c7ab6
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-08-15 10:57:16 -0700

    Build up Redis queue.

--- dags/vlass_find_work.py
+++ dags/vlass_find_work.py
@@ -1,6 +1,11 @@
+import logging
+import requests
+
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.http_hook import HttpHook
+from urllib import parse as parse
 from datetime import datetime, timedelta
 
 default_args = {
@@ -16,42 +21,33 @@ default_args = {
 }
 
 redis_hook = RedisHook(redis_conn_id='redis_default')
+http_conn = HttpHook('GET', 'test_netrc')
 
 # TODO - when deploying to have this actually run, catchup=True!!!!!
 # and schedule_interval=timedelta(hours=1)
 
 vlass_find_work = DAG('vlass_find_work', default_args=default_args,
-                      catchup=True,
-                      schedule_interval=timedelta(hours=1))
+                      catchup=True, schedule_interval=timedelta(hours=1))
 
-
-# provide_context must be true to get the kwargs values
+# provide_context in default_args above must be True to get the kwargs values
 def query_vlass(ds, **kwargs):
     prev_date = kwargs['prev_execution_date'].to_datetime_string()
     next_date = kwargs['next_execution_date'].to_datetime_string()
     query_meta = "SELECT fileName FROM archive_files WHERE archiveName ='VLASS'" \
                  " AND ingestDate > '{}' and ingestDate <= '{}'".format(
-                    prev_date, next_date)
-    data = {"QUERY": query_meta, "LANG": "ADQL", "FORMAT": "csv"}
-    from urllib import parse as parse
-    url = "http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/ad/auth-sync?{}".format(
-        parse.urlencode(data))
-    import logging
-    logging.error(url)
-    import requests
-    artifact_files_list = requests.get(url, auth=(
-        'goliaths', 'loreenam')).content.decode('utf-8')
-    logging.error(artifact_files_list.split())
-    return artifact_files_list.split()
+                     prev_date, next_date)
+    data = {'QUERY': query_meta, 'LANG': 'ADQL', 'FORMAT': 'csv'}
 
-# caching to redis attempt
+    with http_conn.run('/ad/auth-sync?{}'.format(parse.urlencode(data))) as response:
+        artifact_files_list = response.text.split()
+        logging.error(artifact_files_list)
+        return artifact_files_list
 
-
+# caching to Redis
 def cache_query_result(**context):
     results = context['task_instance'].xcom_pull('vlass_periodic_query')
-    import logging
     logging.error(results)
-    redis_hook.get_conn().set('test_key', results)
+    redis_hook.get_conn().set('vlass_{}'.format(execution_date.strftime('%Y_%m_%d_%H_%M_%S')), results)
 
 
 t1 = PythonOperator(task_id='vlass_periodic_query',
